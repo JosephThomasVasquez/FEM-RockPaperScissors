d42d4a1f55d13d89da3b1f6533bf9fbda99c80cc
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2020-05-29 06:14:12 -0700

    Added symbol svgs and basic styling

--- js/app.js
+++ js/app.js
@@ -1,3 +1,20 @@
 const score = document.querySelector(".score-value");
 
 console.log(score.innerHTML);
+
+
+
+
+
+
+
+// localStorage Solution
+
+// Get localStorage score
+//localStorage.getItem('rpsScore');
+
+// Set localStorage score
+//localStorage.setItem('rpsScore', `${score.innerHTML}`);
+
+// Remove key from localStorage
+//localStorage.removeItem('rpsScore');
