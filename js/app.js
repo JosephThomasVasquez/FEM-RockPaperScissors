0445802fe727cbf33165b531a3e84539d8c006fc
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2020-05-30 12:51:48 -0700

    Updated button symbols and converting html to javascript

--- js/app.js
+++ js/app.js
@@ -1,7 +1,25 @@
 const score = document.querySelector(".score-value");
+const bodyHTML = document.querySelector("body");
 
 console.log(score.innerHTML);
 
+
+// Create the main div container with the title and score
+const createMainContainer = () => {
+    const mainContainer = document.createElement('div');
+    mainContainer.classList.add('container', 'main');
+    bodyHTML.appendChild(mainContainer);
+
+    const titleHTML = document.createElement('div');
+    titleHTML.classList.add('title');
+    mainContainer.appendChild(titleHTML);
+
+    const h2HTML = document.createElement('h2');
+    h2HTML.classList.add('text-title');
+    titleHTML.appendChild(h2HTML);
+};
+
+createMainContainer();
 
 
 
