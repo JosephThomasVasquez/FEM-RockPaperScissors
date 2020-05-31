1cd9aa60f5286ffd352322e860bced42aba6a6bf
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2020-05-30 20:09:39 -0700

    Added the main container style and html elements using Javascript

--- js/app.js
+++ js/app.js
@@ -14,9 +14,46 @@ const createMainContainer = () => {
     titleHTML.classList.add('title');
     mainContainer.appendChild(titleHTML);
 
-    const h2HTML = document.createElement('h2');
-    h2HTML.classList.add('text-title');
-    titleHTML.appendChild(h2HTML);
+    const scoreHTML = document.createElement('div');
+    scoreHTML.classList.add('score');
+    mainContainer.appendChild(scoreHTML);
+
+    const scoreElement = document.createElement('h3');
+    scoreElement.classList.add('text-score');
+    scoreElement.innerText = 'SCORE';
+    scoreHTML.appendChild(scoreElement);
+    
+    const scoreAmount = document.createElement('h2');
+    scoreAmount.classList.add('score-value');
+    scoreAmount.innerText = '12';
+    scoreHTML.appendChild(scoreAmount);
+
+    // Create h2 Elements and append to Title element
+    const createH2Element = (element, name) => {
+        const h2HTML = document.createElement(`${element}`);
+        h2HTML.classList.add('text-title');
+        h2HTML.innerText = `${name}`;
+        titleHTML.appendChild(h2HTML);
+
+        // On mouseover - Add styling
+        h2HTML.addEventListener('mouseover', (e) => {
+
+            if (e.target.innerText === 'ROCK') {
+                e.target.innerHTML = '> ROCK';
+                e.target.classList.add('choose-text');
+            }else if (e.target.innerText === '> ROCK') {
+                e.target.innerHTML = 'ROCK';
+                e.target.classList.remove('choose-text');
+            };
+        });
+
+    };
+
+    createH2Element('h2', 'ROCK');
+    createH2Element('h2', 'PAPER');
+    createH2Element('h2', 'SCISSORS');
+
+
 };
 
 createMainContainer();
