f34dd36b82c8e23cd2de27a9eb6f2482da4794be
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2020-05-31 17:26:26 -0700

    Added animation and updated styling and design

--- js/app.js
+++ js/app.js
@@ -1,47 +1,15 @@
-const bodyHTML = document.querySelector("body");
-const score = document.querySelector(".score-value");
+const bodyHTML = document.querySelector('body');
+const scoreHTML = document.querySelector('.text-score');
 const pickerContainer = document.querySelector('.picker-container');
-
-console.log(score.innerHTML);
-
 
 // Create the main div container with the title and score
 const createMainContainer = () => {
-    const mainContainer = document.createElement('div');
-    mainContainer.classList.add('container', 'main');
-    bodyHTML.appendChild(mainContainer);
-
-    const titleHTML = document.createElement('div');
-    titleHTML.classList.add('title');
-    mainContainer.appendChild(titleHTML);
-
-
-    // Score container div
-    const scoreHTML = document.createElement('div');
-    scoreHTML.classList.add('score');
-    mainContainer.appendChild(scoreHTML);
-
-    const scoreElement = document.createElement('h3');
-    scoreElement.classList.add('text-score');
-    scoreElement.innerText = 'SCORE';
-    scoreHTML.appendChild(scoreElement);
+    
     
     const scoreAmount = document.createElement('h2');
     scoreAmount.classList.add('score-value');
     scoreAmount.innerText = '12';
     scoreHTML.appendChild(scoreAmount);
-
-    // Create h2 Elements and append to Title element
-    const createH2Element = (element, name) => {
-        const h2HTML = document.createElement(`${element}`);
-        h2HTML.classList.add('text-title');
-        h2HTML.innerText = `${name}`;
-        titleHTML.appendChild(h2HTML);
-    };
-
-    createH2Element('h2', 'ROCK');
-    createH2Element('h2', 'PAPER');
-    createH2Element('h2', 'SCISSORS');
 
 
 };
@@ -75,10 +43,17 @@ const gameContainer = () => {
     ];
 
     const createSymbol = (symbol) => {
+
         const symbolPick = document.createElement('div');
         const symbolRing = document.createElement('div');
         const symbolImg = document.createElement('img');
-        symbolPick.classList.add('pick-symbol', 'col-6');
+        
+        if (symbol.value === 1) {
+            symbolPick.classList.add('pick-symbol', 'col-12');
+        }else {
+            symbolPick.classList.add('pick-symbol', 'col-6');
+        }
+        
         symbolRing.classList.add(symbol.ring);
         symbolImg.classList.add(symbol.css);
         symbolImg.src = symbol.image;
